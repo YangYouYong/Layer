b75ca08ecb0429bea6756df2e7615137c0a14ca4
youyong.yang <youyong.yang@vechain.com>
2018-04-08 18:11:21 +0800

    Initial Commit

---
+++ layerT/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  layerT
+//
+//  Created by yangyouyong on 2018/4/8.
+//  Copyright © 2018年 yangyouyong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ layerT/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  layerT
+//
+//  Created by yangyouyong on 2018/4/8.
+//  Copyright © 2018年 yangyouyong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
